f51831bfd4eeae509eed7a6769e6994cfead2e18
Truly Mittal <trulymittal@gmail.com>
2019-08-08 00:40:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lottieapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lottieapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView lottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lottieAnimationView = findViewById(R.id.lottieAnimationView2);
+
+
+    }
+}
